1b4bc543b6dec83283be340968f65a71a0cf7349
Milan <milanchristenhusz@gmail.com>
2024-09-18 19:45:27 +0200

    We make sure it sends the latest user data to the emulator

--- app/Controllers/Player/Client/ClientController.php
+++ app/Controllers/Player/Client/ClientController.php
@@ -3,18 +3,43 @@
 namespace App\Controllers\Player\Client;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class ClientController extends BaseController
 {
+    protected $userModel;
+    protected $session;
+
+    public function __construct() {
+        $this->userModel = model('UserModel');
+        $this->session = session();
+    }
+
     public function index(): string
     {
         return view('player/client');
     }
 
     public function getUserData() {
-        if (session()->has('user')) {
-            $userData = session()->get('user');
-            return $this->response->setJSON($userData);
+        
+        if ($this->session->has('user')) {
+            $userId = $this->session->get('user')->id;
+            
+            $userData = $this->userModel->find($userId);
+
+            if ($userData) {
+                $userData = (object) $userData;
+
+                unset(
+                    $userData->mail, 
+                    $userData->password, 
+                    $userData->account_created
+                );
+
+                return $this->response->setJSON($userData);
+            } else {
+                return $this->response->setJSON(['error' => 'User not found']);
+            }
         } else {
             return $this->response->setJSON(['error' => 'User not logged in']);
         }
